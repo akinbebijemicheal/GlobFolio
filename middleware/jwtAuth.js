2373aa068cba8ea3c39d293634d99e47854640d1
Raqeeb Taiwo <44765711+taiworoqeeb@users.noreply.github.com>
2022-03-29 11:35:20 +0100

    fix for api

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = function (req, res, next) {
-    const token = req.header('x-auth-token');
+    const token = req.header('Authorization');
     if(!token) {
         return res.status(401).send({
             success: false,
