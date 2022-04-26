2e2eb9220ebc218e19ce0751e7dd458416fdb722
Raqeeb Taiwo <taiworoqeeb@gmail.com>
2022-04-26 14:47:53 +0100

    -a

--- util/multer.js
+++ util/multer.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb){
-        cb(null, path.resolve(__dirname, './uploads/'))
+        cb(null, path.resolve(__dirname, 'uploads'))
     },
     filename: function(req, file, cb){
         cb(null, (new Date).toISOString().substr(0, 10)+ '-' + file.originalname)
