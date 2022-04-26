86be50b2c076031797deb3423db9b2addc5a610c
Raqeeb Taiwo <taiworoqeeb@gmail.com>
2022-04-26 14:50:58 +0100

    -a

--- util/multer.js
+++ util/multer.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
         cb(null, path.resolve(__dirname, 'uploads'))
     },
     filename: function(req, file, cb){
-        cb(null, (new Date).toISOString().substr(0, 10)+ '-' + file.originalname)
+        cb(null, file.originalname)
     }
 });
 
