fbc96ba1fc9cd6a2709a46d34b468588995222f1
Raqeeb Taiwo <taiworoqeeb@gmail.com>
2022-06-28 13:49:15 +0100

    -a

--- util/multer.js
+++ util/multer.js
@@ -1,28 +1,37 @@
-const multer = require('multer');
-const path = require('path')
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, path.resolve(__dirname, './uploads'))
-    },
-    filename: function(req, file, cb){
-        cb(null, (new Date).toISOString().substr(0, 10)+ '-' + file.originalname)
-    }
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, "./uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString().substr(0, 10) + "-" + file.originalname);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/JPEG' || file.mimetype === 'image/jpg'|| file.mimetype === 'image/png' || file.mimetype === 'image/gif' || file.mimetype === 'image/svg' || file.mimetype === 'image/JPG' || file.mimetype === 'image/PNG' || file.mimetype === 'image/GIF' || file.mimetype === 'image/SVG'){
-        cb(null, true)
-    }else{
-        cb({message: 'Unsupported file format'}, false)
-    }
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/JPEG" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/gif" ||
+    file.mimetype === "image/svg" ||
+    file.mimetype === "image/JPG" ||
+    file.mimetype === "image/PNG" ||
+    file.mimetype === "image/GIF" ||
+    file.mimetype === "image/SVG"
+  ) {
+    cb(null, true);
+  } else {
+    cb({ message: "Unsupported file format" }, false);
+  }
 };
 
-
 const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter
+  storage: storage,
+  fileFilter: fileFilter,
 });
 
 module.exports = upload;
-
