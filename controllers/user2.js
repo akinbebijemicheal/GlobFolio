3daccb5103e67b8cd9303e9d07d38f37fb2c464b
Raqeeb Taiwo <taiworoqeeb@gmail.com>
2022-06-30 23:18:48 +0100

    -a

--- controllers/user2.js
+++ controllers/user2.js
@@ -5,20 +5,6 @@ const jwt = require('jsonwebtoken');
 const passport = require('passport');
 require('dotenv').config();
 const nodemailer = require('nodemailer')
-
-/*const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
-
-const myOAuth2Client = new OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    )
-
-myOAuth2Client.setCredentials({
-        refresh_token: process.env.REFRESH_TOKEN
-});
-
-const myAccessToken = myOAuth2Client.getAccessToken()*/
 const baseurl = process.env.BASE_URL
 
 exports.RegisterUser = async (role, req, res) => {
@@ -51,7 +37,6 @@ exports.RegisterUser = async (role, req, res) => {
         user = new User({
             email,
             password: hashedPass,
-            verified: verify,
             email_verify: emailVerified
         });
     
@@ -59,312 +44,6 @@ exports.RegisterUser = async (role, req, res) => {
 
         req.flash('success', "Registration successful")
          res.redirect(`login-${role}`);
-
-        if(role === 'vendor'){
-          if(serviceType === "food" ){
-                const newRest = new Restaurant({
-                    userId: Newuser.id,
-                    restaurant: Newuser.business,
-                    contact_no: Newuser.phone_no
-                })
-                await newRest.save()
-          }
-
-      };
-
-//          user = await User.findOne({ where: {
-//             email: email
-//         }})
-        
-//             const token = jwt.sign({email: user.email}, process.env.TOKEN, { expiresIn: "15m"});
-//             const link = `${process.env.BASE_URL}/email-verification?userId=${user.id}&token=${token}`;
-
-//                 var transporter = nodemailer.createTransport({
-//                     host: process.env.EMAIL_HOST,
-//                         port: process.env.EMAIL_PORT,
-//                         secure: true, // true for 465, false for other ports
-//                         tls: {
-//                         rejectUnauthorized: false,
-//                         },
-//                         auth: {
-//                         user: process.env.EMAIL_USERNAME, // generated ethereal user
-//                         pass: process.env.EMAIL_PASSWORD, // generated ethereal password
-//                         },
-//                   });
-        
-            
-//             let fname = user.fullname.split(' ')
-//             const mailOptions = {
-//                 from:  `${process.env.E_TEAM}`,
-//                 to: `${user.email}`,
-//                 subject: "Deepend",
-//                 html: `
-//                 <!DOCTYPE html>
-// <html>
-// <head>
-
-//   <meta charset="utf-8">
-//   <meta http-equiv="x-ua-compatible" content="ie=edge">
-//   <title>Email Confirmation</title>
-//   <meta name="viewport" content="width=device-width, initial-scale=1">
-//   <style type="text/css">
-//   /**
-//    * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
-//    */
-//   @media screen {
-//     @font-face {
-//       font-family: 'Source Sans Pro';
-//       font-style: normal;
-//       font-weight: 400;
-//       src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
-//     }
-//     @font-face {
-//       font-family: 'Source Sans Pro';
-//       font-style: normal;
-//       font-weight: 700;
-//       src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
-//     }
-//   }
-//   /**
-//    * Avoid browser level font resizing.
-//    * 1. Windows Mobile
-//    * 2. iOS / OSX
-//    */
-//   body,
-//   table,
-//   td,
-//   a {
-//     -ms-text-size-adjust: 100%; /* 1 */
-//     -webkit-text-size-adjust: 100%; /* 2 */
-//   }
-//   /**
-//    * Remove extra space added to tables and cells in Outlook.
-//    */
-//   table,
-//   td {
-//     mso-table-rspace: 0pt;
-//     mso-table-lspace: 0pt;
-//   }
-//   /**
-//    * Better fluid images in Internet Explorer.
-//    */
-//   img {
-//     -ms-interpolation-mode: bicubic;
-//   }
-//   /**
-//    * Remove blue links for iOS devices.
-//    */
-//   a[x-apple-data-detectors] {
-//     font-family: inherit !important;
-//     font-size: inherit !important;
-//     font-weight: inherit !important;
-//     line-height: inherit !important;
-//     color: inherit !important;
-//     text-decoration: none !important;
-//   }
-//   /**
-//    * Fix centering issues in Android 4.4.
-//    */
-//   div[style*="margin: 16px 0;"] {
-//     margin: 0 !important;
-//   }
-//   body {
-//     width: 100% !important;
-//     height: 100% !important;
-//     padding: 0 !important;
-//     margin: 0 !important;
-//   }
-//   /**
-//    * Collapse table borders to avoid space between cells.
-//    */
-//   table {
-//     border-collapse: collapse !important;
-//   }
-//   a {
-//     color: #1a82e2;
-//   }
-//   img {
-//     height: auto;
-//     line-height: 100%;
-//     text-decoration: none;
-//     border: 0;
-//     outline: none;
-//   }
-//   </style>
-
-// </head>
-// <body style="background-color: #e9ecef;">
-
-//   <!-- start preheader -->
-//   <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
-//     Deepend Email Verification
-//   </div>
-//   <!-- end preheader -->
-
-//   <!-- start body -->
-//   <table border="0" cellpadding="0" cellspacing="0" width="100%">
-
-//     <!-- start logo -->
-//     <tr>
-//       <td align="center" bgcolor="#e9ecef">
-//         <!--[if (gte mso 9)|(IE)]>
-//         <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-//         <tr>
-//         <td align="center" valign="top" width="600">
-//         <![endif]-->
-//         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-//           <tr>
-//             <td align="center" valign="top" style="padding: 36px 24px;">
-//               <a href=${baseurl} target="_blank" style="display: inline-block;">
-//                 <img src=${baseurl}/images/deep.png alt="Logo" border="0" width="60" style="display: flex; width: 60px; max-width: 60px; min-width: 60px;">
-//               </a>
-//             </td>
-//           </tr>
-//         </table>
-//         <!--[if (gte mso 9)|(IE)]>
-//         </td>
-//         </tr>
-//         </table>
-//         <![endif]-->
-//       </td>
-//     </tr>
-//     <!-- end logo -->
-
-//     <!-- start hero -->
-//     <tr>
-//       <td align="center" bgcolor="#e9ecef">
-//         <!--[if (gte mso 9)|(IE)]>
-//         <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-//         <tr>
-//         <td align="center" valign="top" width="600">
-//         <![endif]-->
-//         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-//           <tr>
-//             <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-//               <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Confirm Your Email Address</h1>
-//             </td>
-//           </tr>
-//         </table>
-//         <!--[if (gte mso 9)|(IE)]>
-//         </td>
-//         </tr>
-//         </table>
-//         <![endif]-->
-//       </td>
-//     </tr>
-//     <!-- end hero -->
-
-//     <!-- start copy block -->
-//     <tr>
-//       <td align="center" bgcolor="#e9ecef">
-//         <!--[if (gte mso 9)|(IE)]>
-//         <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-//         <tr>
-//         <td align="center" valign="top" width="600">
-//         <![endif]-->
-//         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-
-//           <!-- start copy -->
-//           <tr>
-//             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-//       <h2> Hi ${fname[0]}, </h2>        
-//               <p style="margin: 0;"> You Have Successfully created an account with Deepend. Tap the button below to confirm your email address. If you didn't create an account with Deepend, you can safely delete this email.</p>
-//             </td>
-//           </tr>
-//           <!-- end copy -->
-
-//           <!-- start button -->
-//           <tr>
-//             <td align="left" bgcolor="#ffffff">
-//               <table border="0" cellpadding="0" cellspacing="0" width="100%">
-//                 <tr>
-//                   <td align="center" bgcolor="#ffffff" style="padding: 12px;">
-//                     <table border="0" cellpadding="0" cellspacing="0">
-//                       <tr>
-//                         <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-//                           <a href=${link} target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
-//                         </td>
-//                       </tr>
-//                     </table>
-//                   </td>
-//                 </tr>
-//               </table>
-//             </td>
-//           </tr>
-//           <!-- end button -->
-
-//           <!-- start copy -->
-//           <tr>
-//             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-//               <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
-//               <p style="margin: 0;"><a href=${link} target="_blank">${link}</a></p>
-//             </td>
-//           </tr>
-//           <!-- end copy -->
-
-//           <!-- start copy -->
-//           <tr>
-//             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
-//               <p style="margin: 0;">Cheers,<br> Deepend Team</p>
-//             </td>
-//           </tr>
-//           <!-- end copy -->
-
-//         </table>
-//         <!--[if (gte mso 9)|(IE)]>
-//         </td>
-//         </tr>
-//         </table>
-//         <![endif]-->
-//       </td>
-//     </tr>
-//     <!-- end copy block -->
-
-//     <!-- start footer -->
-//     <tr>
-//       <td align="center" bgcolor="#e9ecef" style="padding: 24px;">
-//         <!--[if (gte mso 9)|(IE)]>
-//         <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-//         <tr>
-//         <td align="center" valign="top" width="600">
-//         <![endif]-->
-//         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-
-//           <!-- start permission -->
-//           <tr>
-//             <td align="center" bgcolor="#e9ecef" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-//               <p style="margin: 0;">You received this email because we received a request for signing up for your DEEPEND account. If you didn't request signing up you can safely delete this email.</p>
-//             </td>
-//           </tr>
-//           <!-- end permission -->
-
-//         </table>
-//         <!--[if (gte mso 9)|(IE)]>
-//         </td>
-//         </tr>
-//         </table>
-//         <![endif]-->
-//       </td>
-//     </tr>
-//     <!-- end footer -->
-
-//   </table>
-//   <!-- end body -->
-
-// </body>
-// </html>`
-//             };
-
-//             transporter.sendMail(mailOptions, function(err, info) {
-//                 if(err){
-//                     console.log(err)
-//                     req.flash('error', "An error occured")
-//                 } else {
-//                     console.log(info);
-//                     req.flash('success', "Registration successful")
-//                     res.redirect(`login-${role}`)
-//                 }
-//             });
 
     }catch(error){
         console.error(error)
@@ -383,16 +62,13 @@ exports.webLoginUser = async (role, req, res) => {
             email: email }
         });
         if(!user){
-
             res.status(404)
-           req.flash("warning", 'User does not exist');
-           res.redirect("back")
-
+            req.flash("warning", 'User does not exist');
+            res.redirect("back")
         }
 
         if(user.role !== role){
-           // res.status(401)
-          req.flash("warning", "Please ensure you are logging-in from the right portal" );
+          req.flash("warning", "Please ensure you are logging-in from the right portal");
           res.redirect("back")
         }
         
@@ -414,10 +90,8 @@ exports.webLoginUser = async (role, req, res) => {
                 role: user.role,
                 phone_no: user.phone_no,
                 country: user.country,
-                serviceType: user.serviceType,
                 address: user.address,
                 expiresIn: '24 hours',
-                verified: user.verified,
                 email_verify: user.email_verify,
                 updatedAt: user.updatedAt,
                 createdAt: user.createdAt,
@@ -463,18 +137,16 @@ exports.userAuth = passport.authenticate('jwt', {session: true});
 
 
 exports.profile = user => {
-   return {
+     return {
         id: user.id,
         fullname: user.fullname,
         phone_no: user.phone_no,
         country: user.country,
-        serviceType: user.serviceType,
         email: user.email,
         address: user.address,
-        sub_status: user.sub_status,
         updatedAt: user.updatedAt,
         createdAt: user.createdAt
-       };
+    };
 
 };
 
@@ -525,15 +197,8 @@ exports.getUser = async (req, res) => {
 };
 
 exports.updateUser = async(req, res) => {
-    //const { fullname, email, phone_no, country, serviceType} = req.body
     try{
-        await User.update( /* {
-            fullname: fullname,
-            phone_no: phone_no,
-            country: country,
-            serviceType: serviceType,
-            email: email,
-        }*/ req.body, { where: 
+        await User.update( req.body, { where: 
             {
             email: req.user.email
             }
