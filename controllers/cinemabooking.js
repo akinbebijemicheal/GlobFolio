55f81fd23f020d01dbe4c1e87d90c2278846053f
akinbebijemicheal <michealakinbebije@gmail.com>
2022-08-05 10:32:19 -0700

    new recennnndddjndnffvffdddnnn

--- controllers/cinemabooking.js
+++ controllers/cinemabooking.js
@@ -42,7 +42,7 @@ exports.bookCinema = async(req, res, next)=>{
             snackQuantity = 1
         }
 
-        if(snacksId != "" || snacksId != null){
+        if(snacksId == null){
             var snack = await Snack.findOne({
                 where:{
                     id: snacksId
