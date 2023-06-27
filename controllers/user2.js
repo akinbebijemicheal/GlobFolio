f1f6b767f24713c34fcde4474856e0aec7618400
Akinbebije Micheal <michealakinbebije@gmail.com>
2023-06-27 19:01:03 +0100

    Ff

--- controllers/user2.js
+++ controllers/user2.js
@@ -140,7 +140,7 @@ exports.webLoginAdmin = async (req, res, next) => {
   }
 };
 
-exports.userAuth = passport.authenticate('jwt', {failureRedirect: "/login-admin", failureFlash: true});
+exports.userAuth = passport.authenticate('jwt', {session: true, failureRedirect: "/register-admin", failureFlash: true});
 
 
 exports.profile = user => {
