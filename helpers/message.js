2dd277ec73c3375f239753d242f81c1e138e0434
Akinbebije Micheal <michealakinbebije@gmail.com>
2023-07-25 12:22:01 +0100

    gg

--- helpers/message.js
+++ helpers/message.js
@@ -613,7 +613,7 @@ let name = first_name
   <table style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:separate;border-spacing:0px;border-radius:4px;background-color:#FFFFFF" width="100%" cellspacing="0" cellpadding="0" bgcolor="#ffffff" role="presentation">
   <tr style="border-collapse:collapse">
   <td class="es-m-txt-l" bgcolor="#ffffff" align="left" style="Margin:0;padding-top:20px;padding-bottom:20px;padding-left:30px;padding-right:30px"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:lato, 'helvetica neue', helvetica, arial, sans-serif;line-height:27px;color:#666666;font-size:18px">Hi, <strong>${name}</strong><br><p style="font-size: 1.4em;">This is to inform you that you just initiated a password change action.</p><br/>
-                    <p style="font-size: 1.4em;">To reset your password, you have input the token below in the app!</p>
+                    <p style="font-size: 1.4em;">To reset your password, input the token below in the app!</p>
                     <p style="font-size: 1.4em;">If this was not initiated by you, do not proceed.</p>.</p></td>
   </tr>
   <tr style="border-collapse:collapse">
