44ddaf319d0eb44215949a0a06cf0fa8353fd238
Akinbebije Micheal <michealakinbebije@gmail.com>
2023-07-25 17:31:17 +0100

    gg

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
+const User = require("../model/user");
 require("dotenv").config();
 
-module.exports = function (req, res, next) {
+module.exports = async (req, res, next) => {
   const token = req.header("Authorization");
   if (!token) {
     return res.status(401).send({
@@ -14,6 +15,19 @@ module.exports = function (req, res, next) {
   try {
     const decoded = jwt.verify(token, process.env.TOKEN);
     //console.log(decoded.user);
+       let user = await User.findOne({
+        where: {
+          id: decoded.user.id,
+        },
+      });
+
+      if(!user){
+           return res.status(401).send({
+             success: false,
+             message: "Token is not valid as user not found",
+           });
+      }
+
     req.user = decoded.user;
     next();
   } catch (error) {
